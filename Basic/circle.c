25c3ddb4182697fd2595e73fe73a21fe2902be5b
Kanha Choubey <kanhaarmyoo7@gmail.com>
2023-09-28 18:38:52 +0530

    C basic

---
+++ Basic/circle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float pi = 3.14, r;
+    printf("Enter the radius of a circle: ");
+    scanf("%f", &r);
+    printf("\n Area of circle is : %f", pi * r);
+    return 0;
+}
---
+++ Basic/index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("enter a number");
+    scanf("%d", &a);
+    printf("enter b number");
+    scanf("%d", &b);
+
+    printf("value of total number is : %d", a + b);
+    return 0;
+}
